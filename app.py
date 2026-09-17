481449bf0f12395380d86a1426a90d3204cbdaa1
tkroosen-web <tkroosen@gmail.com>
2026-09-17 17:05:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -937,7 +937,7 @@ with st.sidebar:
         f"""
         <div style="display:flex;align-items:center;gap:10px;">
             <div style="
-                width:38px;height:38px;border-radius:50%;
+                width:38px;height:50px;border-radius:50%;
                 background:#2563EB;color:white;
                 display:flex;align-items:center;justify-content:center;
                 font-weight:700;">
